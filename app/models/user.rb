1c6f2cc32bda5303adf3a3e186c7acbf7a47a80e
Tim Kohlstadt <Tim@MacBook-Pro.local>
2018-09-14 15:42:24 +0200

    Better user validation when using SEEDS

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,11 +20,11 @@ class User < ApplicationRecord
   validates :password, length: { in: 6..20 }, if: :password_required?
 
   validates :language, presence: true
-  validates :role_ids, presence: true
-  validates :org_unit_ids, presence: true
+  validates :role_ids, presence: true, if: :seeds_blank?
+  validates :org_unit_ids, presence: true, if: :seeds_blank?
 
   # Validate if there is at least one admin
-  validate :at_least_one_admin?
+  validate :at_least_one_admin?, if: :seeds_blank?
 
   def password_required?
     !persisted? || !password.nil? || !password_confirmation.nil?
@@ -32,8 +32,12 @@ class User < ApplicationRecord
 
   private
 
+  def seeds_blank?
+    ENV['SEEDS'].blank?
+  end
+
   def at_least_one_admin?
-    if not (User.with_role :admin).exists? and ENV['SEEDS'].blank?
+    if not (User.with_role :admin).exists?
       errors.add(:min_one_admin, I18n.t('errors.messages.admin'))
     end
   end
