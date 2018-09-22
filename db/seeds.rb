65d67b3337b90606c7a0edfc434d5a608ba93367
Tim Kohlstadt <Tim@MacBook-Pro.local>
2018-09-22 12:27:06 +0200

    Add hint in 'db/seeds.rb' were the default values were located

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,2 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# The data to to seed the database with its default values were located in 'config/initializers/knx4r.rb'.
+# It can then be loaded while starting the webserver with the 'RAILS_ENV=production SEEDS=1 HOST_IP=own_ip_address rails s' command.
